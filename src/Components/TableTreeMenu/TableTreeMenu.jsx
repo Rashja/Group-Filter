af9bcb68bf4f19a735f39efda23413a4cf876a8d
Ramtin <ramtinsharif@gmail.com>
2019-07-17 16:09:30 +0430

    final commit functionality

--- src/Components/TableTreeMenu/TableTreeMenu.jsx
+++ src/Components/TableTreeMenu/TableTreeMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 //material-ui
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -6,12 +6,6 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import Grid from "@material-ui/core/Grid";
 
 //Data
 import initialRows from './TableTreeMenuApi';
@@ -19,6 +13,7 @@ import initialRows from './TableTreeMenuApi';
 const TreeTable = () => {
   /*----------------------------------- States --------------------------------*/
   let [rows,setRows] = useState([]);
+  let [child,setChild] = useState([]);
   /*----------------------------------- Effect --------------------------------*/
   useEffect(()=>{
     setRows(getNestedChildren(initialRows,undefined))
@@ -39,50 +34,31 @@ const TreeTable = () => {
      return out
  }
   /*--------------------------------------------------------------------------------*/
-  const renderNested = (row,index) => {
+  const handleChild = (id) => {
+      if(child.includes(id)){
+        setChild(child=child.filter(filterRow=>
+            id !== filterRow
+        ))
+      }else{
+        setChild([...child,id])
+      }
+  }
+  /*--------------------------------------------------------------------------------*/
+  const renderNested = (rows) => {
     return(
         <>
-        {row.children === undefined ?
-            <TableRow key={index}>
-              <TableCell align="left">{row.Name}</TableCell>
-              <TableCell align="left">{row.Gender}</TableCell>
-              <TableCell align="left">{row.Age}</TableCell>
-              <TableCell align="left">{row.Homeland}</TableCell>
-              <TableCell align="left">{row.Grade}</TableCell>
-            </TableRow>
-            :
-            <TableRow key={index}>
-                <TableCell style={{padding:0}} colspan='5' align="left">
-              <ExpansionPanel key={index} style={{ background: "#fafafa" }}>
-                  <ExpansionPanelSummary
-                    key={index}
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                  >
-                    <Typography component={"span"} key={index}>
-                    <TableRow key={index}>
-                        <TableCell align="left">{row.Name}</TableCell>
-                        <TableCell align="left">{row.Gender}</TableCell>
-                        <TableCell align="left">{row.Age}</TableCell>
-                        <TableCell align="left">{row.Homeland}</TableCell>
-                        <TableCell align="left">{row.Grade}</TableCell>
+            {rows.map(row=>
+                <> 
+                    <TableRow>
+                        <TableCell id={row.id} align="left">{row.Name}</TableCell>
+                        <TableCell id={row.id} align="left">{row.Gender}</TableCell>
+                        <TableCell id={row.id} align="left">{row.Age}</TableCell>
+                        <TableCell id={row.id} align="left">{row.Homeland}</TableCell>
+                        <TableCell id={row.id} align="left">{row.Grade}</TableCell>
                     </TableRow>
-                    </Typography>
-                  </ExpansionPanelSummary>
-                {row.children.map((child,index) =>
-                  <ExpansionPanelDetails key={index}>
-                    <Grid item xs={12}>
-                      <Typography component={"span"} key={index}>
-                        {renderNested(child,index)}
-                      </Typography>
-                    </Grid>
-                  </ExpansionPanelDetails>
-                )}
-              </ExpansionPanel>
-              </TableCell>
-              </TableRow>
-          }
+           { row.children && row.children.length && child.indexOf(row.id) !==-1? renderNested(row.children):null }
+                </>
+            )}
       </>
     )
  }
@@ -114,10 +90,7 @@ const TreeTable = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {rows.map((row, index) =>
-                  renderNested(row,index) 
-                  )
-                }
+                  {renderNested(rows)} 
               </TableBody>
             </Table>
           </Paper>
